efa78458b278b9e1efc0e90f98faf8e780155aa9
Mark Aldritz Dela Virgen <6435411+DiamondCubeMiner@users.noreply.github.com>
2026-07-31 04:59:05 +0000

    Added standalone AI and HSI apps

---
+++ pyG5/pyG5HSI.py
@@ -0,0 +1,148 @@
+"""
+Created on 30 Jul 2026.
+
+@author: Ben Lauret
+"""
+
+__version__ = "0.0.4"
+__appName__ = "pyG5"
+
+import argparse
+import logging
+import sys
+import platform
+
+
+from PySide6.QtCore import (
+    Qt,
+    QTimer,
+    QCoreApplication,
+    QSettings,
+    Signal,
+)
+from PySide6.QtWidgets import QApplication
+
+from pyG5.pyG5Network import pyG5NetWorkManager
+from pyG5.pyG5View import pyG5HSIWidget
+from pyG5.pyG5Main import pyG5BaseWindow
+
+
+class pyG5HSIApp(QApplication):
+    """pyG5HSIApp PySide6 application.
+
+    Args:
+        sys.argv
+
+    Returns:
+        self
+    """
+
+    def __init__(self):
+        """g5Widget Constructor.
+
+        Args:
+            parent: Parent Widget
+
+        Returns:
+            self
+        """
+        QApplication.__init__(self, sys.argv)
+
+        QCoreApplication.setOrganizationName("pyG5")
+        QCoreApplication.setOrganizationDomain("pyg5.org")
+        QCoreApplication.setApplicationName("pyG5")
+        self.settings = QSettings(
+            QSettings.Format.IniFormat,
+            QSettings.Scope.UserScope,
+            QCoreApplication.organizationDomain(),
+            "pyG5",
+        )
+
+        # parse the command line arguments
+        self.argument_parser()
+
+        # set the verbosity
+        if self.args.verbose:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
+
+        logging.info("{} v{}".format(self.__class__.__name__, __version__))
+
+        self.networkManager = pyG5NetWorkManager()
+
+        self.paintTimer = QTimer()
+        self.paintTimer.timeout.connect(
+            self.painTimerCB
+        )  # Let the interpreter run each 500 ms.
+        self.paintTimer.start(25)  # You may change this if you wish.
+
+        # The QWidget widget is the base class of all user interface objects in PySide6.
+        self.mainWindow = pyG5HSIWindow()
+
+        self.networkManager.drefUpdate.connect(
+            self.mainWindow.pyG5HSI.drefHandler
+        )
+
+        # Show window
+        self.mainWindow.loadSettings()
+
+        if platform.machine() == "aarch64":
+            self.mainWindow.setWindowFlags(
+                self.mainWindow.windowFlags() | Qt.FramelessWindowHint
+            )
+            self.mainWindow.setWindowState(Qt.WindowFullScreen)
+
+        self.mainWindow.show()
+
+    def painTimerCB(self):
+        """Trigger update of all the widgets."""
+        self.mainWindow.pyG5HSI.update()
+
+    def argument_parser(self):
+        """Initialize the arguments passed from the command line."""
+        self.parser = argparse.ArgumentParser(
+            description="{} Application v{}".format(__appName__, __version__)
+        )
+        self.parser.add_argument(
+            "-v", "--verbose", help="increase verbosity", action="store_true"
+        )
+
+        self.args = self.parser.parse_args()
+
+
+class pyG5HSIWindow(pyG5BaseWindow):
+    """pyG5HSIApp PySide6 application.
+
+    Args:
+        sys.argv
+
+    Returns:
+        self
+    """
+
+    closed = Signal()
+
+    def __init__(self, parent=None):
+        """g5Widget Constructor.
+
+        Args:
+            parent: Parent Widget
+
+        Returns:
+            self
+        """
+        pyG5BaseWindow.__init__(self, parent)
+
+        self.pyG5HSI = pyG5HSIWidget()
+
+        self.setCentralWidget(self.pyG5HSI)
+
+
+if __name__ == "__main__":
+    """Main application."""
+    a = pyG5HSIApp()
+
+    sys.exit(a.exec())
+
+    pass
